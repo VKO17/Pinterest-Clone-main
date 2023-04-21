64a37ef2a38ca2c424d6715ff5268cd5272d8dca
VKO17 <darksidevko17@gmail.com>
2023-04-20 20:59:23 -0600

    update css  and js  of the searchbar

--- SearchBar/searchBar.script.js
+++ SearchBar/searchBar.script.js
@@ -1,14 +1,22 @@
+function toggleSection() {
+    var section = document.querySelector('.search-section');
+    if (section.classList.contains('show-section')) {
+        section.classList.remove('show-section');
+    } else {
+        section.classList.add('show-section');
+    }
+  }
 
-const inicio = document.getElementById("inicio");
-const hoy = document.getElementById("hoy");
+  const inicio = document.getElementById("inicio");
+  const hoy = document.getElementById("hoy");
 
-inicio.addEventListener("click", () => {
-  hoy.classList.remove("active");
-  inicio.classList.add("active");
-});
+  inicio.addEventListener("click", () => {
+    hoy.classList.remove("active");
+    inicio.classList.add("active");
+  });
 
-hoy.addEventListener("click", () => {
-  inicio.classList.remove("active");
-  hoy.classList.add("active");
-});
+  hoy.addEventListener("click", () => {
+    inicio.classList.remove("active");
+    hoy.classList.add("active");
+  });
 
