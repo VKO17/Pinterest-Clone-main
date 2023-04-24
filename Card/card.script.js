ad648157d86f1219e7da3750a5373551abaf1450
VKO17 <darksidevko17@gmail.com>
2023-04-24 17:58:17 -0600

    Update full card-section v0.5 24/04/2023

---
+++ Card/card.script.js
@@ -0,0 +1,125 @@
+const searchSection = document.querySelector(".search-section");
+const closeIcon = document.querySelector(".close-icon");
+const closeIconFaTimes = document.querySelector('.close-icon i.fa-times');
+const modal = document.querySelector(".modal");
+
+// Get the element with class cont-arte-manga
+const contViaje = document.querySelector('.cont-viaje');
+const contArtMan = document.querySelector('.cont-arte-manga');
+
+// Seccion Viajes
+contViaje.addEventListener('click', function () {
+
+  const imgElements = document.querySelectorAll('img');
+
+  imgElements.forEach(function (imgElement) {
+
+    if (!imgElement.classList.contains('viajes-img') && !imgElement.classList.contains('img-searchbar')) {
+
+      imgElement.style.display = "none";
+
+    }
+
+  });
+
+  const containerElement = document.querySelector('.grid-container');
+  const masonryElements = containerElement.querySelectorAll('.grid-item:not([style*="display: none"])');
+
+  let msnry = Masonry.data(containerElement);
+  msnry.destroy();
+
+  const viajesImgElements = document.querySelectorAll('viajes-img');
+  viajesImgElements.forEach(function (viajeImgElement) {
+    viajeImgElement.style.width = "auto";
+  });
+
+  const searchbarImgElements = document.querySelectorAll('.img-searchbar');
+  searchbarImgElements.forEach(function (searchbarImgElement) {
+    // Do nothing to maintain original CSS styles
+  });
+
+  msnry = new Masonry(containerElement, {
+    itemSelector: ".grid-item",
+    columnWidth: 230,
+    gutter: 20,
+    isFitWidth: true
+  });
+
+  searchSection.style.display = "none";
+  modal.style.display = "none";
+  closeIcon.style.opacity = "1";
+  closeIconFaTimes.style.backgroundColor = 'transparent';
+  closeIconFaTimes.style.color = '#333';
+  closeIconFaTimes.style.fontSize = '1.5em';
+
+});
+
+// Seccion Card Arte-Manga
+contArtMan.addEventListener('click', function () {
+
+  // Get all img elements on the page
+  const imgElements = document.querySelectorAll('img');
+
+  // Loop through each image
+  imgElements.forEach(function (imgElement) {
+
+    // If the image doesn't have the class arte-manga-img or img-searchbar
+    if (!imgElement.classList.contains('arte-manga-img') && !imgElement.classList.contains('img-searchbar')) {
+      // Remove the image
+      imgElement.style.display = "none";
+    }
+
+  });
+
+  // Get the container element
+  const containerElement = document.querySelector('.grid-container');
+
+  // Remove the images that have been hidden
+  const masonryElements = containerElement.querySelectorAll('.grid-item:not([style*="display: none"])');
+
+  // Remove the Masonry object
+  let msnry = Masonry.data(containerElement);
+  msnry.destroy();
+
+  // Loop through each viajes-img element and set the width to auto
+  const artManImgElements = document.querySelectorAll('.arte-manga-img');
+  artManImgElements.forEach(function (artManImgElement) {
+    artManImgElement.style.width = "auto";
+  });
+
+  // Loop through each img-searchbar element and set the width to 100%
+  const searchbarImgElements = document.querySelectorAll('.img-searchbar');
+  searchbarImgElements.forEach(function (searchbarImgElement) {
+    // Do nothing to maintain original CSS styles
+  });
+
+  // Initialize Masonry again
+  msnry = new Masonry(containerElement, {
+    itemSelector: ".grid-item",
+    columnWidth: 230,
+    gutter: 20,
+    isFitWidth: true
+  });
+
+  // Hide search section, modal, and show close icon
+  searchSection.style.display = "none";
+  modal.style.display = "none";
+  closeIcon.style.opacity = "1";
+  closeIconFaTimes.style.backgroundColor = 'transparent';
+  closeIconFaTimes.style.color = '#333';
+  closeIconFaTimes.style.fontSize = '1.5em';
+
+});
+
+function closeSearchSection() {
+  location.reload();
+}
+
+// Agregar evento click al icono de cerrar
+const closeIcon2 = document.querySelector('.close-icon');
+closeIcon2.addEventListener('click', closeSearchSection);
+
+// ------------------------------------------------------------------------------------------------------------------------------
+
+
+
--- SearchBar/searchBar.script.js
+++ SearchBar/searchBar.script.js
@@ -1,12 +1,13 @@
 /* Evento del despliegue de la search-bar */
 
-
 // Esperar a que el DOM termine de cargarse
 document.addEventListener('DOMContentLoaded', function () {
 
     // Obtener la sección de búsqueda y la barra de búsqueda
     var searchSection = document.querySelector('.search-section');
     var searchInput = document.querySelector('.search-form input[type="search"]');
+    var modal = document.getElementById('myModal');
+    var span = document.getElementsByClassName("close")[0];
 
     // Agregar un controlador de eventos al documento completo
     document.addEventListener('click', function (event) {
@@ -16,7 +17,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Si el clic ocurrió fuera de la sección de búsqueda, cerrarla y cambiar el color de fondo
             searchSection.classList.remove('open');
-            document.body.style.backgroundColor = '#ffffff';
+            searchSection.style.visibility = 'hidden';
+            // modal desactivado
+            modal.style.display = "none";
         }
     });
 
@@ -26,14 +29,32 @@ document.addEventListener('DOMContentLoaded', function () {
         // Si la sección de búsqueda no está abierta, abrirla y cambiar el color de fondo
         if (!searchSection.classList.contains('open')) {
             searchSection.classList.add('open');
-            document.body.style.backgroundColor = '#999999';
+            searchSection.style.visibility = 'visible';
+            // activado modal
+            modal.style.display = "block";
         }
 
         // Detener la propagación del evento para evitar que se cierre la sección de búsqueda
         event.stopPropagation();
     });
 
-});
+    // Agregar un controlador de eventos a la sección de búsqueda
+    searchSection.addEventListener('click', function (event) {
 
+        // Detener la propagación del evento para evitar que se cierre la sección de búsqueda
+        event.stopPropagation();
 
+        // Si la sección de búsqueda no está abierta, cambiar el color de fondo
+        if (!searchSection.classList.contains('open')) {
+            //modal activado
+            modal.style.display = "block";
+        }
+    });
 
+    // Agregar un controlador de eventos al documento completo para cerrar el modal si se hace clic fuera de él
+    window.addEventListener('click', function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});  
